25e0eea4606737547282b9810e8c5d4e4ed3cf1a
Misha Marinenko <misha.marinenko.offical@gmail.com>
2017-06-16 22:00:20 +0300

    v0.1b

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xB3, 0xF2, 0x1D, 0x97, 0xC9, 0xD2, 0x4B, 0xF1, 0x3A, 0xB8, 0xF2, 0xCE, 0xBF, 0xE2, 0x03, 0x34  } };
+}
